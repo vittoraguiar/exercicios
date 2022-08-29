1c9ec76b6cf7f3d2ea0fe9909f8cfb6a2fb7ad77
Vittor de Aguiar <103087952+vittoraguiar@users.noreply.github.com>
2022-08-29 20:14:02 -0300

    Update Aumento_por_cargo.java

--- Java/Aumento_por_cargo.java
+++ Java/Aumento_por_cargo.java
@@ -5,13 +5,11 @@ import java.util.Scanner;
 public class Aumento_por_cargo {
 
 	public static void main(String[] args) {
-		
 		Scanner sc = new Scanner(System.in);
 		char criterio = 's';
 		double salario, salarioNovo;
 		
-		while (criterio == 's') {
-			
+		do { 
 			System.out.print("Insira o seu salário em reais: ");
 			salario = sc.nextDouble();
 			System.out.println();
@@ -20,62 +18,82 @@ public class Aumento_por_cargo {
 			System.out.print("Insira o seu cargo: ");
 			char cargo = sc.next().charAt(0);
 			System.out.println();
-			
-			if (cargo == 'G' || cargo == 'g') {
+
+			switch (cargo) {
+			case 'G':
+			case 'g':
 				salarioNovo = Gerente(salario);
-				System.out.println("Como o seu cargo é de Gerente, o seu aumento será de 10%.");
-				System.out.println("Logo, o seu salário que antes era de R$" + String.format("%.2f", salario) + " passará "
-						+ "a ser de R$" + String.format("%.2f", salarioNovo) + "!\nA diferença entre os salários é de "
-								+ "R$" + String.format("%.2f", (salarioNovo - salario)) + ".");
-			}
-			else if(cargo == 'E' || cargo == 'e') {
+				System.out.println("Como o seu cargo eh de Gerente, o seu aumento sera de 10%.");
+				System.out.printf("Logo, o seu salario que antes era de R$ %.2f", salario);
+				System.out.printf("Passara a ser de R$ %.2f", salarioNovo);
+				System.out.printf("A diferenca entre os salarios eh de R$ %.2f", (salarioNovo - salario));
+				break;
+
+			case 'E':
+			case 'e':
 				salarioNovo = Engenheiro(salario);
-				System.out.println("Como o seu cargo é de Engenheiro, o seu aumento será de 20%.");
-				System.out.println("Logo, como o seu salário era de R$" + String.format("%.2f", salario) + " passará a ser "
-						+ "de R$" + String.format("%.2f", salarioNovo) + "!\nA diferença entre os salários é de R$"
-						+ String.format("%.2f", (salarioNovo - salario)));
-			}
-			else if(cargo == 'T' || cargo == 't') {
+				System.out.println("Como o seu cargo eh de Engenheiro, o seu aumento sera de 20%.");
+				System.out.printf("Logo, o seu salario que antes era de R$ %.2f", salario);
+				System.out.printf("Passara a ser de R$ %.2f", salarioNovo);
+				System.out.printf("A diferenca entre os salarios eh de R$ %.2f", (salarioNovo - salario));
+				break;
+
+			case 'T':
+			case 't':
 				salarioNovo = Tecnico(salario);
-				System.out.println("Como o seu cargo é de Técnico, o seu aumento será de 30%.");
-				System.out.println("Logo, como o seu salário era de R$" + String.format("%.2f", salario) + " passará a ser "
-						+ "de R$" + String.format("%.2f", salarioNovo) + "!\nA diferença entre os salários é de R$"
-						+ String.format("%.2f", (salarioNovo - salario)));
-			}
-			else if(cargo == 'O' || cargo == 'o') {
+				System.out.println("Como o seu cargo eh de Tecnico, o seu aumento sera de 30%.");
+				System.out.printf("Logo, o seu salario que antes era de R$ %.2f", salario);
+				System.out.printf("Passara a ser de R$ %.2f", salarioNovo);
+				System.out.printf("A diferenca entre os salarios eh de R$ %.2f", (salarioNovo - salario));
+				break;
+
+			case 'O':
+			case 'o':
 				salarioNovo = Outro(salario);
-				System.out.println("Como o seu cargo é outro, o seu aumento será de 40%.");
-				System.out.println("Logo, como o seu salário era de R$" + String.format("%.2f", salario) + " passará a ser "
-						+ "de R$" + String.format("%.2f", salarioNovo) + "!\nA diferença entre os salários é de R$"
-						+ String.format("%.2f", (salarioNovo - salario)));
+				System.out.println("Como o seu cargo eh Outro, o seu aumento sera de 40%.");
+				System.out.printf("Logo, o seu salario que antes era de R$ %.2f", salario);
+				System.out.printf("Passara a ser de R$ %.2f", salarioNovo);
+				System.out.printf("A diferenca entre os salarios eh de R$ %.2f", (salarioNovo - salario));
+				break;
+
+			default:
+				System.out.println("Cargo Invalido!");
 			}
-			else {
-				System.out.println("Cargo inválido!");
-			}
+
+			// Verificação do critério
 			System.out.print("\nTecle \"s\" para realizar uma nova consulta: ");
 			criterio = sc.next().charAt(0);
-		}
-		if(criterio != 's') {
+			
+		if (criterio != 's')
+		{
 			System.out.println("\nFim do programa. Obrigado!\n");
 		}
-		sc.close();
+			
+		}while(criterio=='s');
+
+	sc.close();
+
 	}
 
+	// Funções
 	public static double Gerente(double x) {
 		double resultado;
 		resultado = (x * 0.10) + x;
 		return resultado;
 	}
+
 	public static double Engenheiro(double x) {
 		double resultado;
 		resultado = (x * 0.20) + x;
 		return resultado;
 	}
+
 	public static double Tecnico(double x) {
 		double resultado;
 		resultado = (x * 0.30) + x;
 		return resultado;
 	}
+
 	public static double Outro(double x) {
 		double resultado;
 		resultado = (x * 0.40) + x;
