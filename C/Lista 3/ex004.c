f39c1b5db5062c0be08921fba0e1147192528b9e
Vittor de Aguiar <103087952+vittoraguiar@users.noreply.github.com>
2022-08-30 11:18:43 -0300

    resolucao correta

--- C/Lista 3/ex004.c
+++ C/Lista 3/ex004.c
@@ -8,17 +8,28 @@
 
 int main(int argc, char const *argv[])
 {
-    int num, numFib;
-    numFib = 1;
+    int num, n1, n2, res;
+    n1 = 1;
+    n2 = 1;
+    res = 1;
     printf("Escreva um numero: ");
     scanf("%d", &num);
 
-    for (int i = 0; i < count; i++)
+    printf("%d ", res);
+    res = n1 + n2;
+
+    while (res <= num)
     {
-        numFib += numFib;
+        n1 = res;
+        res = res + n2;
+        n2 = n1;
+        if (res <= num)
+        {
+            printf("%d ", res);
+        }
+        else
+            break;
     }
-    
-
-
+    printf("\nFim do programa...\n\n");
     return 0;
 }
