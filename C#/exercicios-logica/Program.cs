94d55cd38722d93288b679a52d58f15125f8e700
Vittor de Aguiar <103087952+vittoraguiar@users.noreply.github.com>
2022-09-05 08:55:49 -0300

    Add files via upload

---
+++ C#/exercicios-logica/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ex001
+{
+    // Fazer um programa para ler um número inteiro, e depois dizer se este número é negativo ou não.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            Console.Write("Insira um número inteiro: ");
+            num = int.Parse(Console.ReadLine());
+            if (num >= 0)
+            {
+                Console.WriteLine("O número inserido é positivo!");
+            }
+            else
+            {
+                Console.WriteLine("O número inserido é negativo");
+            }
+        }
+    }
+}
