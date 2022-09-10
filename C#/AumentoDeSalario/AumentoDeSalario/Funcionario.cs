168649fe1dd9763338f4481b640e7ac3a50ca188
Vittor de Aguiar <103087952+vittoraguiar@users.noreply.github.com>
2022-09-10 11:51:21 -0300

    Add files via upload

---
+++ C#/AumentoDeSalario/AumentoDeSalario/Funcionario.cs
@@ -0,0 +1,23 @@
+namespace AumentoDeSalario;
+
+public class Funcionario
+{
+    public string Nome;
+    public double SalarioBruto;
+    public double Imposto;
+
+    public double SalarioLiquido()
+    {
+        return SalarioBruto - Imposto;
+    }
+
+    public void AumentarSalario(double porcentagem)
+    {
+        SalarioBruto = SalarioBruto + (SalarioBruto * porcentagem / 100);
+    }
+
+    public override string ToString()
+    {
+        return Nome + ", " + SalarioLiquido().ToString("C");
+    }
+}
---
+++ C#/AumentoDeSalario/AumentoDeSalario/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace AumentoDeSalario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario func = new Funcionario();
+
+            Console.WriteLine("Insira os dados do funcionário:");
+            Console.Write("Nome: ");
+            func.Nome = Console.ReadLine()!;
+            Console.Write("Salário bruto: ");
+            func.SalarioBruto = double.Parse(Console.ReadLine()!);
+            Console.Write("Imposto: ");
+            func.Imposto = double.Parse(Console.ReadLine()!);
+            
+            Console.WriteLine("Funcionário: " + func);
+            Console.Write("Digite a porcentagem para aumentar o salário: ");
+            double porcent = double.Parse(Console.ReadLine()!);
+            func.AumentarSalario(porcent);
+            Console.WriteLine("\nDados atualizados: " + func);
+        }
+    }
+}
